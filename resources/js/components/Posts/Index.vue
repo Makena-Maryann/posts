31cdabf907b57b9bf2593554d170e215ef36a0b1
Makena-Maryann <maryann.makena00@gmail.com>
2021-09-18 15:08:15 +0300

    Static Vue Component

---
+++ resources/js/components/Posts/Index.vue
@@ -0,0 +1,43 @@
+<template>
+   <div>
+       <table class="table">
+        <thead>
+            <tr>
+                <th>
+                    Title
+                </th>
+                <th>
+                    Post text
+                </th>
+                <th>
+                    Created date
+                </th>
+                <th>Actions</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td>Something</td>
+                <td>Longer text</td>
+                <td>2021-07-18</td>
+                <td></td>
+            </tr>
+            <tr>
+                <td>Something</td>
+                <td>Longer text</td>
+                <td>2021-07-22</td>
+                <td></td>
+            </tr>
+            <tr>
+                <td>Something</td>
+                <td>Longer text</td>
+                <td>2021-07-28</td>
+                <td></td>
+            </tr>
+        </tbody>
+    </table>  
+   </div>
+</template>
+
+<script>
+</script>
